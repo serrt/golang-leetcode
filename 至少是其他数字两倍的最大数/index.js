5729566bbbca32200fd03859cdff6d7f48add46c
serrt <1163816051@qq.com>
2020-05-11 10:08:19 +0800

    至少是其他数字两倍的最大数

--- 至少是其他数字两倍的最大数/index.js
+++ 至少是其他数字两倍的最大数/index.js
@@ -1,3 +1,11 @@
+/**
+ * 在一个给定的数组nums中，总是存在一个最大元素
+ * 查找数组中的最大元素是否至少是数组中每个其他数字的两倍
+ * 如果是，则返回最大元素的索引，否则返回-1
+ * 
+ * @param {number[]} nums
+ * @return {number}
+ */
 var dominantIndex = function(nums) {
     var max = arrayMax(nums)
     var maxValue = max[0]
